408e294b97b502067696d667c809014f89561026
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-02-24 19:23:29 -0300

    Fetch posts from API
    Fetch posts from API and using Axios library and setting up the posts state

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios'
+
 function App() {
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = () => {
+    Axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        setPosts(response.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <div>
       Alkemy challenge individual
-    </div>
+    </div>   
   );
 }
 
