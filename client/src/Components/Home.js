fee12b07fcb6d904dbf9d3d63af7a0635e14ae01
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-02-25 00:39:34 -0300

    Print posts and add PropTypes
    Print posts from Post components and add PropTypes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import Post from './Post'
+import PropTypes from 'prop-types';
 
-function Home() {
+function Home({posts}) {
     return (
         <div>
-            Home
+            {posts.map(post => {
+                return (
+                    <Post key={post.id} postId={post.id} postTitle={post.title}/>
+                )
+            })}
         </div>
     )
 }
+
+Home.defaultProps = {
+    posts: [],
+  };
+  
+  Home.propTypes = {
+    posts: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      concept: PropTypes.string,
+      amount: PropTypes.number,
+      date: PropTypes.string,
+      type: PropTypes.string,
+    })),
+  };
+
 
 export default Home
