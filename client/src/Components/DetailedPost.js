85ac33150e7ba32f374b46824b24b22b05f1ea37
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-02-25 11:19:35 -0300

    Create Not Found Post funcionality
    Create Not Found Post funcionality that prints "not found post" when there's no post with such post id

--- client/src/Components/DetailedPost.js
+++ client/src/Components/DetailedPost.js
@@ -5,9 +5,11 @@ import PropTypes from 'prop-types';
 function DetailedPost(props) {
     const [blogId, setBlogId] = useState('')
     const [selectedPost, setSelectedPost] = useState([])
+    const [postNotFoundDisplay, setPostNotFoundDisplay] = useState('')
     
     useEffect(() => {
         setBlogId(props.match.params.postId)
+        props.match.params.postId >= 100 ? setPostNotFoundDisplay('') : setPostNotFoundDisplay('none')
     }, [])
 
     useEffect(() => {
@@ -18,6 +20,7 @@ function DetailedPost(props) {
     return (
         <div className="card text-center">
   <div className="card-body">
+    <p style={{display: postNotFoundDisplay}}>Post no encontrado.</p>
     <h5 className="card-title">{selectedPost.title}</h5>
     <p className="card-text">{selectedPost.body}</p>
     <a href={`/edit/${blogId}`} className="btn btn-warning" style={{marginRight: '5px'}}>Editar</a>
