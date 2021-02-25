9c5c13c1218c61a4b6ff57877a8f6d8c14414110
Valentin Torres <66843444+valentintorres02@users.noreply.github.com>
2021-02-25 00:07:06 -0300

    Add nabvar items
    Add Home an Edit navbar items.

--- client/src/Components/Nabvar.js
+++ client/src/Components/Nabvar.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 function Nabvar() {
     return (
-        <div>
-            Nabvar
-        </div>
+        <ul className="nav">
+      <li className="nav-item">
+        <a className="nav-link active" href="/">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/edit">Editar</a>
+      </li>
+    </ul>
     )
 }
 
